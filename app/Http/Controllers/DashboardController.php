9329b4bfd389df334f259fb3620f01fb49febcd7
Javier Agut <javier.agut@avannubo.com>
2023-05-25 11:38:52 +0200

    ..

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $response = Http::withOptions(['verify' => false])->get('https://quirky-mahavira.217-76-154-49.plesk.page/api/productos');
+        $responseData = $response->json();
+        $data = null; 
+        
+        if (isset($responseData['data'])) {
+            $data = $responseData['data'];
+        }
+    
+        if (!$data) {
+            $message = "Todavía no hay productos agregados.";
+            return view('dashboard', compact('message'));
+        } else {
+            return view('dashboard', compact('data'));
+        }
+    }
+    
+    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
